bd13cb790830bda48da8da5f70226149be733844
audemar.agtong95@gmail.com <audemar.agtong95@gmail.com>
2022-07-13 23:56:01 +0800

    Update 3

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+//Prevent user from desktop mode
+window.desktopcheck = function() {
+    var check = false;
+        
+     if(window.innerWidth>768){
+        check=true;
+     }
+     return check;
+}
+if(window.desktopcheck()){
+    document.getElementsByTagName("body")[0].style.display = "none";
+    alert("Webpage does not support wider screen more than 768 pixels!\nTo use this webpage in your desktop, drag the corner of the border of your browser and make it smaller. Thank your...");
+}
