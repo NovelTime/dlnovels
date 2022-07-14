855a30ea68872176626d8be474a479afe99556b6
audemar.agtong95@gmail.com <audemar.agtong95@gmail.com>
2022-07-14 20:50:10 +0800

    Update 7

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ if(window.desktopcheck()){
     document.getElementsByTagName("body")[0].style.display = "none";
     alert("Webpage does not support wider screen more than 768 pixels!\nTo use this webpage in your desktop, drag the corner of the border of your browser and make it smaller. Thank your...");
 }
+//QR code
+let qrCode = document.getElementById("qrcode");
+qrCode.setAttribute("src","");
+qrCode.src = "icon/qrcode.png";
