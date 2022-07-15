e381c580de2cfe6c6b16714526e9a0a8c0ec7ec3
audemar.agtong95@gmail.com <audemar.agtong95@gmail.com>
2022-07-16 06:56:03 +0800

    Update 9.5

--- main.js
+++ main.js
@@ -11,5 +11,8 @@ if(window.desktopcheck()){
     document.getElementsByTagName("body")[0].style.display = "none";
     alert("Webpage does not support wider screen more than 768 pixels!\nTo use this webpage in your desktop, drag the corner of the border of your browser and make it smaller. Thank your...");
 }
-let qrCode = document.getElementById('qrcode');
-qrCode.setAttribute("src","https://noveltime.github.io/dlnovels/icon/qrcode.png");
+
+$('#qrcode').attr('src','https://noveltime.github.io/dlnovels/icon/qrcode.png');
+
+var webnovel = $("<a>",{href:"webnovel.html", class:"w3-bar-item w3-btn"},"</a>").text('Web Novel');
+$("#mySidebar").append(webnovel);
