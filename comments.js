beddf4b0dead89391a1023c6f868fe13c136e5bf
audemar.agtong95@gmail.com <audemar.agtong95@gmail.com>
2022-07-15 17:55:30 +0800

    Update 9.1

---
+++ comments.js
@@ -0,0 +1,29 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBF3gd9Tn9uRwvuWDY0_glpcmFrwOPhZAY",
+    authDomain: "web-dnovels.firebaseapp.com",
+    projectId: "web-dnovels",
+    storageBucket: "web-dnovels.appspot.com",
+    messagingSenderId: "644268467476",
+    appId: "1:644268467476:web:c35e6edf0a21f78820ce01",
+    measurementId: "G-V49VBD5MR7"
+};
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+
+var database = firebase.database();
+
+function sendMessage(){
+  var name = document.getElementById('Name').value;
+  var message = document.getElementById('message').value;
+  
+  var newMessageKey = database.ref().child('comments').push().key;
+  database.ref('comments/'+newMessageKey+'Name').set(name);
+  database.ref('comments/'+newMessageKey+'Message').set(message);
+}
+
+document.getElementById('commentsForm').addEventListener('submit',sendComment);
+function sendComment(){
+  e.preventDefault();
+}
--- main.js
+++ main.js
@@ -11,5 +11,5 @@ if(window.desktopcheck()){
     document.getElementsByTagName("body")[0].style.display = "none";
     alert("Webpage does not support wider screen more than 768 pixels!\nTo use this webpage in your desktop, drag the corner of the border of your browser and make it smaller. Thank your...");
 }
-let qrCode = document.getElementById("qrcode");
+let qrCode = document.getElementById('qrcode');
 qrCode.setAttribute("src","https://noveltime.github.io/dlnovels/icon/qrcode.png");
